cf62f2a71e11844b54df231803ea675659a13437
Anthony Underwood <au3@sanger.ac.uk>
2019-04-17 11:11:16 +0000

    use lower coverage

--- run_seroba.sh
+++ run_seroba.sh
@@ -5,9 +5,9 @@ cat - > /tmp/assembly.fas
 # merge all contigs into a single concatenated seqeunce
 grep -v "^>" /tmp/assembly.fas | awk 'BEGIN { ORS=""; print ">merged_contigs\n" } { print }' > /tmp/assembly_merged.fas
 # simulate reads from assembly
-pirs simulate -l 100 -x 50 -m 500 -o assembly /tmp/assembly_merged.fas  > /dev/null 2>&1
+pirs simulate -l 100 -x 10 -m 500 -o assembly /tmp/assembly_merged.fas  > /dev/null 2>&1
 # run seroba
-seroba runSerotyping /seroba/database assembly_100_500_1.fq assembly_100_500_2.fq assembly  > /dev/null 2>&1
+seroba runSerotyping  --coverage 2 /seroba/database assembly_100_500_1.fq assembly_100_500_2.fq assembly  > /dev/null 2>&1
 # output result
 result=$(awk '{print $2}' assembly/pred.tsv)
 jq --arg key0 'result_type' \
