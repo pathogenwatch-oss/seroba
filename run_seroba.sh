ff97e0aa39476b07f0a62ab185de20b0800d9fc4
Richard Goater <richard.goater27@gmail.com>
2019-07-25 19:42:42 +0100

    Option to set simulated read coverage with PIRS_COVERAGE environment variable

--- run_seroba.sh
+++ run_seroba.sh
@@ -5,8 +5,8 @@ cat - > /tmp/assembly.fas
 # merge all contigs into a single concatenated seqeunce
 grep -v "^>" /tmp/assembly.fas | awk 'BEGIN { ORS=""; print ">merged_contigs\n" } { print }' > /tmp/assembly_merged.fas
 # simulate reads from assembly
-pirs simulate -l 100 -x 50 -m 500 --no-substitution-errors --no-indel-errors --no-gc-content-bias --threads=1 --random-seed=12345 -o assembly /tmp/assembly_merged.fas  > /dev/null 2>&1
+pirs simulate -l 100 -x ${PIRS_COVERAGE:-50} -m 500 --no-substitution-errors --no-indel-errors --no-gc-content-bias --threads=1 --random-seed=12345 -o assembly /tmp/assembly_merged.fas  > /dev/null 2>&1
 # run seroba
-seroba runSerotyping  --coverage 20 /seroba/database assembly_100_500_1.fq assembly_100_500_2.fq assembly  > /dev/null 2>&1
+seroba runSerotyping --coverage 20 /seroba/database assembly_100_500_1.fq assembly_100_500_2.fq assembly  > /dev/null 2>&1
 # write value to STDOUT
 awk -F '\t' '{print $2}' assembly/pred.tsv
