bc33a27e7dd30eb0aa3382ecf2b80c62aac7caba
Richard Goater <richard.goater27@gmail.com>
2019-07-26 12:30:45 +0100

    Fix coverage arithmetic

--- run_seroba.sh
+++ run_seroba.sh
@@ -9,6 +9,6 @@ grep -v "^>" /tmp/assembly.fas | awk 'BEGIN { ORS=""; print ">merged_contigs\n" 
 # simulate reads from assembly
 pirs simulate -l 100 -x $COVERAGE -m 500 --no-substitution-errors --no-indel-errors --no-gc-content-bias --threads=1 --random-seed=12345 -o assembly /tmp/assembly_merged.fas  > /dev/null 2>&1
 # run seroba
-seroba runSerotyping --coverage $[$COVERAGE * 2/5] /seroba/database assembly_100_500_1.fq assembly_100_500_2.fq assembly  > /dev/null 2>&1
+seroba runSerotyping --coverage $[COVERAGE * 2/5] /seroba/database assembly_100_500_1.fq assembly_100_500_2.fq assembly  > /dev/null 2>&1
 # write value to STDOUT
 awk -F '\t' '{print $2}' assembly/pred.tsv
